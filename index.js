0a824b05f55cfa28e6d1f402ba54bc9f28554c76
muhammed ramees <muhammedramees09876@gmail.com>
2024-01-19 19:31:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+ //importing json-server dependency/package
+
+ const jsonServer=require('json-server')
+
+ //create server
+
+ const server=jsonServer.create()
+
+ ///create middleware
+
+ const middleware=jsonServer.defaults()
+
+ //initailizing router with resource
+ const router =jsonServer.router('db.json')
+
+ //implementing middleware to server
+ server.use(middleware)
+
+ //implementing router to server
+ server.use(router)
+
+ //defining PORT number
+
+ const PORT=process.env.PORT || 4000
+
+ //activating server into listening for requests
+
+ server.listen(PORT,()=>{
+    console.log("server is running in PORT:",PORT)
+ })
